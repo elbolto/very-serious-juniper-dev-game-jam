fe89af38d0b001b43736ae640c0231acc1f8ac43
Sebastian Hein <sebastian.hein@unity3d.com>
2026-06-20 19:07:45 +0200

    added basic player movement logic

--- spin-to-win/Assets/spin-to-win/Scripts/CharacterMovement.cs
+++ spin-to-win/Assets/spin-to-win/Scripts/CharacterMovement.cs
@@ -3,10 +3,17 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
+    Camera _camera; 
+    Rigidbody2D _rigidbody; 
+
+    [Range(0, 10)]
+    public float thrust;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
-        
+        _camera = Camera.main;
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void OnDisable()
@@ -15,28 +22,17 @@ public class CharacterMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Mouse mouse = Mouse.current;
-        if (mouse.leftButton.wasPressedThisFrame)
-        {
-           Vector3 mousePosition = mouse.position.ReadValue();
-        //    Ray ray = m_Camera.ScreenPointToRay(mousePosition);
-        //    if (Physics.Raycast(ray, out RaycastHit hit))
-        //    {
-        //        // Use the hit variable to determine what was clicked on.
-        //    }
-            Debug.Log("Mouse Clicked");
-        }
-    }
-
-    void OnClick()
-    {
-        
-    }
-
     void FixedUpdate()
     {
-        
+        Mouse mouse = Mouse.current;
+        if (mouse.leftButton.isPressed)
+        {
+            Vector3 mousePosition = mouse.position.ReadValue();
+            Vector3 clickWorldPos = _camera.ScreenToWorldPoint(mousePosition);
+            Vector3 pos = transform.position; 
+
+            Vector3 forceDirection = pos - clickWorldPos;
+            _rigidbody.AddForce(forceDirection * thrust);
+        }
     }
 }
