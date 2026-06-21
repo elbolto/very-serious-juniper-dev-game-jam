0013d594f62f2ed8b1b21e5c20a035e4359e8a1f
Jonathan Hein <johe5877@bht-berlin.de>
2026-06-21 14:53:15 +0200

    Implement Attachment logic, the spaceship swings at points now
    TODO: refine swinging so that the spaceship can swing the rescueship

--- spin-to-win/Assets/spin-to-win/Scripts/AttachmentRadius.cs
+++ spin-to-win/Assets/spin-to-win/Scripts/AttachmentRadius.cs
@@ -6,8 +6,8 @@ public class AttachmentRadius : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         //get necessary components attachmentSystem from ship and target from parent of collider object
-        AttachmentSystem connection = col.GetComponent<AttachmentSystem>();
-        Rigidbody2D target = transform.parent.GetComponent<Rigidbody2D>();
+        AttachmentSystem connection = transform.parent.GetComponent<AttachmentSystem>();
+        Rigidbody2D target = col.GetComponent<Rigidbody2D>();
 
         //call Attach() from AttachmentSystem, create joint and draw line
         connection.Attach(target);
--- spin-to-win/Assets/spin-to-win/Scripts/AttachmentSystem.cs
+++ spin-to-win/Assets/spin-to-win/Scripts/AttachmentSystem.cs
@@ -1,12 +1,13 @@
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AttachmentSystem : MonoBehaviour
 {
     private DistanceJoint2D _joint; //same distance between two objects
     private LineRenderer _line; //draw line (temporary, later there is going to be a sprite)
-    public bool isConnected = false;
+    private Rigidbody2D _target;
 
+    public bool isConnected { get; private set; } //read-only from outside class
     void Awake()
     {
         //set line renderer at runtime and set thickness etc.
@@ -20,31 +21,52 @@ public class AttachmentSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_joint == null)
+        if (_target == null) 
         {
             return;
         }
         //connect line between two points
         _line.SetPosition(0, transform.position);
-        _line.SetPosition(1, _joint.connectedBody.position);
+        _line.SetPosition(1, _target.position);
+        
+        //detach on space bar
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Detach();
+        }
     }
 
     public void Attach(Rigidbody2D target)
     {
         //if already connected return, there wont be any double connections
-        if(_joint != null)
+        if(isConnected)
         {
             return;
         }
+        _target = target;
 
         isConnected = true;
 
         //add joint at runtime, connect to target and let distance be calculated automatically
-        _joint = gameObject.AddComponent<DistanceJoint2D>();
-        _joint.connectedBody = target;
-        _joint.autoConfigureDistance = true;
+        _joint = target.gameObject.AddComponent<DistanceJoint2D>();
+        _joint.connectedBody = GetComponent<Rigidbody2D>();
+        _joint.autoConfigureDistance = false;
+        _joint.distance = Vector2.Distance(transform.position, target.position);
 
         //enable line
         _line.enabled = true;
+    }
+
+    public void Detach()
+    {
+        if(!isConnected)
+        {
+            return;
+        }
+        
+        Destroy(_joint);
+        _target = null;
+        isConnected = false;
+        _line.enabled = false; 
     }
 }
